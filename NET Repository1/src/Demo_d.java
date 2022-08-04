4f3a287e165436a88a42dc33b0bdca6cc1a60db3
HP <HP@192.168.0.8>
2022-08-04 17:47:30 +0530

    By sd

---
+++ NET Repository1/src/Demo_d.java
@@ -0,0 +1,19 @@
+
+public class Demo_d 
+
+
+
+ {
+	
+	public void g1()
+	{
+		System.out.println("Start");
+	}
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+      
+	}
+
+}
